0e852995ea9df4b96c556a35d723f2f6fb62ea8b
sam ghoreishi <sam20gh@gmail.com>
2019-01-22 10:57:30 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -24,7 +24,10 @@ def end_game(card_total)
 end
 
 def initial_round
-  # code #initial_round here
+  deal_one = deal_card
+  deal_two = deal_card
+  card_total = deal_one + deal_two
+  display_card_total(card_total)
 end
 
 def hit?

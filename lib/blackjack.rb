c6d0129a729edbe80b0c5e6cba089e0fb1c7183b
sam ghoreishi <sam20gh@gmail.com>
2019-01-22 10:58:57 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -31,7 +31,11 @@ def initial_round
 end
 
 def hit?
-  # code hit? here
+  prompt_user
+  input = get_user_input
+  if input == 'h'
+    card_total = card_total + deal_card
+  end
 end
 
 def invalid_command
